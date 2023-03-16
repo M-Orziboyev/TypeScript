abfc1dbd489a384894cb9addfa317f610093a095
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-03-16 17:37:32 +0500

    basic theories

---
+++ script.js
@@ -0,0 +1,16 @@
+//array ***************
+var color = ['red', 'black', 'blue'];
+console.log(color);
+var colorsNum = ['red', 12, 'blue'];
+console.log(colorsNum);
+//any *****************
+var smth = 1;
+console.log(smth);
+smth = 'SM';
+console.log(smth);
+smth = ['black', 'white'];
+smth = {
+    name: "Victor",
+    age: '12'
+};
+console.log(smth);
---
+++ script.ts
@@ -0,0 +1,23 @@
+//array ***************
+const color: string[] = ['red', 'black', 'blue']
+console.log(color);
+
+const colorsNum: (string | number)[] = ['red', 12, 'blue']
+console.log(colorsNum);
+
+//any *****************
+
+let smth: any = 1;
+console.log(smth);
+
+smth = 'SM';
+console.log(smth);
+
+smth = ['black', 'white']
+
+smth = {
+    name: "Victor",
+    age: '12'
+}
+console.log(smth);
+
